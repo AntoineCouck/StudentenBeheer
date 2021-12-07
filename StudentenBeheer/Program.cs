735f7e54e4aa83b3e86f5fb12f3c9ad8456d8b71
AntoineCouck <74922305+AntoineCouck@users.noreply.github.com>
2021-12-07 12:15:57 +0100

    add

--- StudentenBeheer/Program.cs
+++ StudentenBeheer/Program.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
+using NETCore.MailKit.Infrastructure.Internal;
 using StudentenBeheer.Areas.Identity.Data;
 using StudentenBeheer.Data;
+using StudentenBeheer.Services.GroupSacePrep.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = (builder.Configuration.GetConnectionString("StudentenBeheerContext"));
@@ -36,7 +40,19 @@ builder.Services.Configure<IdentityOptions>(options =>
 var app = builder.Build();
 
 // password settings
+builder.Services.AddTransient<IEmailSender, MailKitEmailSender>();
+builder.Services.Configure<MailKitOptions>(options =>
+{
+    options.Server = builder.Configuration["ExternalProviders:MailKit:SMTP:Address"];
+    options.Port = Convert.ToInt32(builder.Configuration["ExternalProviders:MailKit:SMTP:Port"]);
+    options.Account = builder.Configuration["ExternalProviders:MailKit:SMTP:Account"];
+    options.Password = builder.Configuration["ExternalProviders:MailKit:SMTP:Password"];
+    options.SenderEmail = builder.Configuration["ExternalProviders:MailKit:SMTP:SenderEmail"];
+    options.SenderName = builder.Configuration["ExternalProviders:MailKit:SMTP:SenderName"];
 
+    // Set it to TRUE to enable ssl or tls, FALSE otherwise
+    options.Security = false;  // true zet ssl or tls aan
+});
 
 
 // Configure the HTTP request pipeline.
