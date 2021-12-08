edd9912c1b46d301b2cd77cd2d84277139d66a4a
AntoineCouck <74922305+AntoineCouck@users.noreply.github.com>
2021-12-08 22:07:56 +0100

    test

--- StudentenBeheer/Models/Student.cs
+++ StudentenBeheer/Models/Student.cs
@@ -5,8 +5,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StudentenBeheer.Models
 {
-    //[Authorize(Roles = "Admin")]
-    [Authorize]
+    [Authorize(Roles = "Admin")]
+    //[Authorize]
     public class Student
     {
 
