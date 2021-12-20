cfef1a76bcd3528c64caf97b347e78601403affe
AntoineCouck <74922305+AntoineCouck@users.noreply.github.com>
2021-12-20 19:25:20 +0100

    Update ApplicationUser.cs

--- StudentenBeheer/Models/ApplicationUser.cs
+++ StudentenBeheer/Models/ApplicationUser.cs
@@ -14,5 +14,11 @@ public class ApplicationUser : IdentityUser
     public string LanguageId { get; set; }
     public Language? Language { get; set; }
 
+
+    public Student ?  Student { get; set; }
+
+    [ForeignKey("Student")]
+    public int ? StudentId { get; set; }
+
 }
 
